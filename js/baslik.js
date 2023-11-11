4b3d88f2916cce306dc23b1813205024c5bba2bc
Bekir <bekir911@hotmail.com>
2023-11-11 14:17:48 +0300

    arrow href kötü yol?

--- js/baslik.js
+++ js/baslik.js
@@ -14,7 +14,7 @@ function func() {
     category.parentElement.insertBefore(category, category.parentElement.children[1]);
 
     // ilk/son sayfa tuşlarını ekle
-    // addPageButtons();
+    addPageButtons();
 }
 
 function addPageButtons() {
@@ -22,19 +22,47 @@ function addPageButtons() {
         if (group.children.length != 5) {
             var bodyColor = getComputedStyle(document.body).color;
             var pageList = qs(group, ".dropdown-menu");
+
+
+            // var leftArrow = dom.ce("a");
+            // var rightArrow = dom.ce("a");
+            // leftArrow.outerHTML = '<a class="btn btn-sm btn-secondary loadcenter" href="null"><i class="fa fa-angle-double-left"></i></a>';
+            // rightArrow.outerHTML = '<a class="btn btn-sm btn-secondary loadcenter" href="null"><i class="fa fa-angle-double-right"></i></a>';
+
+            // leftArrow.href = group.firstElementChild.href ?? undefined;
+
+
+
+
             var leftArrow = dom.clone(group.firstElementChild);
             var rightArrow = dom.clone(group.firstElementChild);
             leftArrow.firstElementChild.className = "fa fa-angle-double-left";
             rightArrow.firstElementChild.className = "fa fa-angle-double-right";
-            leftArrow.href = pageList.firstElementChild.href;
-            rightArrow.href = pageList.lastElementChild.href;
 
-            if (group.firstElementChild.href.length === 0) {
-                leftArrow.style.color = bodyColor;
-            }
-            if (group.lastElementChild.href.length === 0) {
-                rightArrow.style.color = bodyColor;
-            }
+            // leftArrow.href = group.firstElementChild.href
+            //     ? pageList.firstElementChild.href
+            //     : undefined;
+            // rightArrow.href = group.lastElementChild.href
+            //     ? pageList.lastElementChild.href
+            //     : undefined;
+
+            group.firstElementChild.href ? leftArrow.href = pageList.firstElementChild.href : null;
+            group.lastElementChild.href ? rightArrow.href = pageList.lastElementChild.href : null;
+
+
+
+
+
+
+            // leftArrow.href = pageList.firstElementChild.href;
+            // rightArrow.href = pageList.lastElementChild.href;
+
+            // if (group.firstElementChild.href.length === 0) {
+            //     leftArrow.style.color = bodyColor;
+            // }
+            // if (group.lastElementChild.href.length === 0) {
+            //     rightArrow.style.color = bodyColor;
+            // }
 
             group.prepend(leftArrow);
             group.append(rightArrow);
