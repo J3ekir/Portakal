a4863cd3d10d5a0ce2135c655feff2af6417cdd0
Yunus İşleyen <dekamir@outlook.com>
2024-01-23 18:02:12 +0300

    Geçmişe başlıkları göm, Çevrimiçi Yazarlar kapanış animasyonunu ayarla

--- js/jqueryEdits.js
+++ js/jqueryEdits.js
@@ -5,6 +5,8 @@ if (!/^https:\/\/normalsozluk\.com\/external/.test(location.href)) {
         adjustTitleInfoAnims();
         adjustScrollToBottom();
         adjustHistoryState();
+        adjustHamburgerToOnlineAnimation();
+        overridePushHistory();
     });
 }
 
@@ -82,6 +84,60 @@ function adjustHistoryState(centerframe) {
     });
 }
 
+function adjustHamburgerToOnlineAnimation() {
+    const elem = document.querySelector("#hamburger-menu .frame-toggler");
+    elem.onclick = null;
+    $(elem).click(function (e) {
+        if (!window.__cfRLUnblockHandlers)
+            return false;
+        $('#hamburger-menu').fadeOut(100, 'swing');
+    });
+}
+
+
+function overridePushHistory() {
+    overrideRenderCenter();
+    overrideLoadCenter();
+
+
+    window.pushHistory = function (e, t, o, title) {
+        if (!(o.length / 1024 > 630)) {
+            var a = ""
+                , r = ""
+                , s = $("<div>" + o + "</div>")
+                , c = s.find(".xhr-mt").first()
+                , d = s.find(".xhr-mdesc").first()
+                , l = s.find(".xhr-cnn").first();
+            a = void 0 !== c && 1 === c.length ? c.html() : title && title.toLowerCase(),
+                void 0 !== d && 1 === d.length && (r = d.html(),
+                    $("meta[name='description']").attr("content", r)),
+                void 0 !== l && 1 === l.length && $('link[rel="canonical"]').attr("href", l.html());
+            var u = {
+                path: e,
+                selector: t,
+                html: o,
+                title: a,
+                desc: r
+            };
+            if ("function" == typeof window.history.pushState) {
+                if (window.history.state && window.history.state.path === e)
+                    return void (window.history.state.html = o);
+                window.history.pushState(u, e, e);
+            } else
+                window.location.hash = "#!" + e;
+            if ("function" == typeof gtag && "" !== window.location.pathname && "/" !== window.location.pathname) {
+                var h = {
+                    page_title: a,
+                    page_location: window.location.href,
+                    page_path: e
+                };
+                gtag("event", "page_view", h);
+            }
+            updateTitle(a);
+        }
+    };
+}
+
 function adjustLoadingIndicator() {
     Object.defineProperty(window, "showLoader", {
         value: function () {
@@ -140,4 +196,53 @@ async function removeJQueryEventListener(event, selector) {
             }
         }, 50);
     });
+}
+
+async function fetchTitle(url) {
+    const response = await fetch(url);
+    const html = await response.text();
+    const match = html.match(/<title>(.*?)<\/title>/);
+    return match ? match[1] : window.windowTitle;
+}
+
+function overrideRenderCenter() {
+    window.renderCenter = function (e, t, title) {
+        $("#centerframe").html(e),
+            isMobile() && collapseLeftFrame(),
+            window.scrollTo(0, 0),
+            pushHistory(t, "#centerframe", e, title),
+            stickPanels(),
+            processEntry(),
+            "undefined" != typeof AUTHOR_ID && initInputEntryBody(),
+            hideLoader();
+    };
+}
+
+function overrideLoadCenter() {
+    window.loadCenter = function (e) {
+        "" !== e && void 0 !== e && ("function" != typeof pageLeaveCheck || pageLeaveCheck() || confirm("bu sayfaya girdiğiniz fakat göndermediğiniz içerikler var. \nsayfadan ayrılırsanız bu içerikler kaybolacaktır. \n\nilerlemek istiyor musunuz?")) && (showLoader(),
+            $.ajax({
+                method: "GET",
+                url: e,
+                cache: !1,
+                async: !0,
+                success: function (t) {
+                    var s = $("<div>" + t + "</div>");
+                    var c = s.find(".xhr-mt").first();
+
+                    if (c !== undefined && c.length === 1) {
+                        renderCenter(t, e);
+                    }
+                    else {
+                        fetchTitle(e).then(title => {
+                            renderCenter(t, e, title);
+                        });
+                    }
+                },
+                error: function (t, i) {
+                    t.status < 500 ? renderCenter(t.responseText, e) : alert("istediğiniz içerik yüklenemedi. \nbir süre sonra yeniden deneyin."),
+                        hideLoader();
+                }
+            }));
+    };
 }
