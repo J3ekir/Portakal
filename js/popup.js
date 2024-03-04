9ca1e0169c1d2fe01b8fe758dacc2ecc498d3292
J3ekir <bekir911@hotmail.com>
2024-03-04 19:18:19 +0300

    Popup'ta seçilen ayarları kaydet

--- js/popup.js
+++ js/popup.js
@@ -5,3 +5,11 @@ chrome.storage.local.get([
     qs("#font-family").value = settings["font-family"];
     qs("#logo").value = settings["logo"];
 });
+
+qs("#font-family").addEventListener("change", event => {
+    chrome.storage.local.set({ "font-family": event.target.value });
+});
+
+qs("#logo").addEventListener("change", event => {
+    chrome.storage.local.set({ "logo": event.target.value });
+});
