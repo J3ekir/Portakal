ab2957b05cf9d9bfadcc6fb282d2ecadc0323878
J3ekir <bekir911@hotmail.com>
2024-03-12 20:38:28 +0300

    Özel yazı tipi ailesi seçmeyi ekle

--- js/popup.js
+++ js/popup.js
@@ -2,12 +2,24 @@ chrome.storage.local.get([
     "font-family",
     "logo",
 ]).then(settings => {
-    qs("#font-family").value = settings["font-family"];
+    qs("#font-family").value = settings["font-family"].replace(/^(other|.*)(.*)/, "$1");
+    qs("#font-family-custom>input").value = settings["font-family"].replace(/^(other|.*)(.*)/, "$2");
     qs("#logo").value = settings["logo"];
 });
 
 qs("#font-family").addEventListener("change", event => {
-    chrome.storage.local.set({ "font-family": event.currentTarget.value });
+    if (event.currentTarget.value === "other") {
+        qs("#font-family-custom>button").click();
+    }
+    else {
+        chrome.storage.local.set({ "font-family": event.currentTarget.value });
+    }
+});
+
+qs("#font-family-custom>button").addEventListener("click", event => {
+    if (event.currentTarget.previousElementSibling.value) {
+        chrome.storage.local.set({ "font-family": `other${ event.currentTarget.previousElementSibling.value }` });
+    }
 });
 
 qs("#logo").addEventListener("change", event => {
