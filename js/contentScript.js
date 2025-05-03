eadbb1e8953f297ffea2e5ee7f3900ba9d0e8750
J3ekir <bekir911@hotmail.com>
2025-05-03 22:37:05 +0300

    Font CSS'ini ayrı fonksiyon yap

--- js/contentScript.js
+++ js/contentScript.js
@@ -106,24 +106,7 @@ function changeGlobalFont() {
 
             const customFont = dom.ce("style");
             customFont.dataset.fontName = fontName;
-            customFont.textContent = `
-                @font-face {
-                    font-family: "${ fontName }";
-                    src: url("${ chrome.runtime.getURL(`fonts/${ fontName }/${ fontName }-latin.woff2`) }") format("woff2");
-                    font-weight: normal;
-                    font-style: normal;
-                    font-display: swap;
-                    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-                }
-                @font-face {
-                    font-family: "${ fontName }";
-                    src: url("${ chrome.runtime.getURL(`fonts/${ fontName }/${ fontName }-latin-ext.woff2`) }") format("woff2");
-                    font-weight: normal;
-                    font-style: normal;
-                    font-display: swap;
-                    unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
-                }
-            `;
+            customFont.textContent = customFontCSS(fontName);
 
             elem.append(
                 link,
@@ -290,6 +273,25 @@ function addPageButtons() {
         group.append(lastPageButton);
     });
 }
+
+const customFontCSS = fontName => `
+    @font-face {
+        font-family: "${ fontName }";
+        src: url("${ chrome.runtime.getURL(`fonts/${ fontName }/${ fontName }-latin.woff2`) }") format("woff2");
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    }
+    @font-face {
+        font-family: "${ fontName }";
+        src: url("${ chrome.runtime.getURL(`fonts/${ fontName }/${ fontName }-latin-ext.woff2`) }") format("woff2");
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+        unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20C0, U+2113, U+2C60-2C7F, U+A720-A7FF;
+    }
+`;
 
 function waitForElement(selector) {
     return new Promise(resolve => {
