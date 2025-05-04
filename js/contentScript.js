5f596c655398c18de661ff4207747ebfade7c800
J3ekir <bekir911@hotmail.com>
2025-05-04 18:41:37 +0300

    Cabin fontunu ekle

--- js/contentScript.js
+++ js/contentScript.js
@@ -19,6 +19,7 @@ const excludedFonts = [
 ];
 
 const fonts = {
+    "Cabin": "https://fonts.googleapis.com/css2?family=Cabin:ital,wdth,wght@0,75..100,400..700;1,75..100,400..700&display=swap",
     "IBM Plex Sans": "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wdth,wght@0,75..100,100..700;1,75..100,100..700&display=swap",
     "Poppins": "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
 };
